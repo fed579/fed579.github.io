63cad9abf6f4acf2ee7381938072f0c3466b36ee
fed579 <51812225+fed579@users.noreply.github.com>
2019-06-18 10:47:53 +0900

    Add files via upload

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,12 @@
 // キャッシュファイルの指定
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-	'/'
+	'/',
+	'/css/style.css',
+	'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js',
+	'/img/icon128.png',
+	'/offline.html',
+	'/img/offline.png'
 ];
 
 // インストール処理
@@ -18,10 +23,76 @@ self.addEventListener('install', function(event) {
 
 
 // リソースフェッチ時のキャッシュロード処理
+//self.addEventListener('fetch', function(event) {
+ //   event.respondWith(
+//        caches.match(event.request).then(function(response) {
+//                return response ? response : fetch(event.request);
+ //           })
+ //   );
+//});
 self.addEventListener('fetch', function(event) {
+  //ブラウザが回線に接続しているかをboolで返してくれる
+  var online = navigator.onLine;
+
+  //回線が使えるときの処理
+  if(online){
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-                return response ? response : fetch(event.request);
-            })
+      caches.match(event.request)
+        .then(
+        function (response) {
+          if (response) {
+            return response;
+          }
+          //ローカルにキャッシュがあればすぐ返して終わりですが、
+          //無かった場合はここで新しく取得します
+          return fetch(event.request)
+            .then(function(response){
+              // 取得できたリソースは表示にも使うが、キャッシュにも追加しておきます
+              // ただし、Responseはストリームなのでキャッシュのために使用してしまうと、ブラウザの表示で不具合が起こる(っぽい)ので、複製しましょう
+              cloneResponse = response.clone();
+              if(response){
+                //ここ&&に修正するかもです
+                if(response || response.status == 200){
+                  //現行のキャッシュに追加
+                  caches.open(CACHE_NAME)
+                    .then(function(cache)
+                    {
+                      cache.put(event.request, cloneResponse)
+                      .then(function(){
+                        //正常にキャッシュ追加できたときの処理(必要であれば)
+                      });
+                    });
+                }else{
+                  //正常に取得できなかったときにハンドリングしてもよい
+                  return response;
+                }
+                return response;
+              }
+            }).catch(function(error) {
+              //デバッグ用
+              return console.log(error);
+            });
+        })
     );
+  }else{
+    //オフラインのときの制御
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // キャッシュがあったのでそのレスポンスを返す
+          if (response) {
+            return response;
+          }
+          //オフラインでキャッシュもなかったパターン
+          return caches.match("offline.html")
+              .then(function(responseNodata)
+              {
+                //適当な変数にオフラインのときに渡すリソースを入れて返却
+                //今回はoffline.htmlを返しています
+                return responseNodata;
+              });
+        }
+      )
+    );
+  }
 });
