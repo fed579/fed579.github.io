ceda8b8d3c99bacbde3dff4697fd1b8c50ee08f2
fed579 <51812225+fed579@users.noreply.github.com>
2019-08-28 12:17:56 +0900

    Update dcm_bnrList_01.js

--- docs/dcm_bnrList_01.js
+++ docs/dcm_bnrList_01.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	var _UA = navigator.userAgent;
 	if (_UA.indexOf('iPhone') > -1 || _UA.indexOf('iPod') > -1 || _UA.indexOf('iPad') > -1) {
 		
-		$.getJSON('output.json' , function(data) {
+		$.getJSON('./output.json' , function(data) {
 			var
 			box1 = $('#dcm_bnrList_box01'),
 			len = data.length;
