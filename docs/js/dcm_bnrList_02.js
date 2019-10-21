ca8bcd487d55b2a06fa299b04df81272429a0bc9
fed579 <51812225+fed579@users.noreply.github.com>
2019-10-21 20:14:32 +0900

    Update dcm_bnrList_02.js

--- docs/js/dcm_bnrList_02.js
+++ docs/js/dcm_bnrList_02.js
@@ -1,20 +1,20 @@
 (function (){
 	var _UA = navigator.userAgent;
 	if (_UA.indexOf('iPhone') > -1 || _UA.indexOf('iPod') > -1 || _UA.indexOf('iPad') > -1) {
-		
-		$.getJSON('https://excurrency.site/no.json', {
+		//$.getJSON('https://fed579.github.io/docs/bnrList_02.json', {
+		$.getJSON('https://stg.media-app.jp/dmenu/no.json', {
 				ts: new Date().getTime()
-			}, function(data) {
+		})
+			
+		.done(function(data) {
 			var
 			box2 = $('#dcm_bnrList_box02'),
-			len = data.length;
-			console.log(len);
 			html = '<ul>';
 			html += '<style>#dcm_bnrList_box02 *{margin:0;padding:0;border:0;box-sizing: border-box;list-styl-type: none;text-decoration: none; box-sizing: content-box;}';
 			html += '#dcm_bnrList_box02{margin: 10px 8px;}';
 			html += '#dcm_bnrList_box02 li{margin-bottom: 5px;} #dcm_bnrList_box02 li:last-child{margin-bottom:0;}';
 			html += '#dcm_bnrList_box02 li a{display: block;}</style>';
-			for(var i = 0; i < len; i++) {
+			for(var i in data) {
 				html += '<li>';
 				html += '<a href="' + data[i].url + '">';
 				html += '<img src="' + data[i].image + '" alt="image" width="100%">';
@@ -23,10 +23,12 @@
 			}
 			html += '</ul>';
 			box2.html(html);
+		})
+		.fail(function(jqXHR, textStatus, errorThrown){
+			$('#dcm_bnrList_box02').hide();
 		});
-	
+
 	}else{
-		$('#dcm_bnrList_box02').hide();
-	
+		$('#dcm_bnrList_box02').hide();	
 	}
 })();
