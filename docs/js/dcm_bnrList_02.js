dc0a348fc4ba164d0e9ea10317cb48a135fa2df6
fed579 <51812225+fed579@users.noreply.github.com>
2019-10-23 12:47:06 +0900

    Update dcm_bnrList_02.js

--- docs/js/dcm_bnrList_02.js
+++ docs/js/dcm_bnrList_02.js
@@ -1,7 +1,7 @@
 (function (){
 	var box2 = $('#dcm_bnrList_box02');
-	var _UA = navigator.userAgent;
-	if (_UA.indexOf('iPhone') > -1 || _UA.indexOf('iPod') > -1 || _UA.indexOf('iPad') > -1) {
+	var _UA = window.navigator.userAgent.toLowerCase();
+	if (_UA.indexOf('iPhone') > -1 || _UA.indexOf('iPod') > -1 || _UA.indexOf('iPad') > -1 || _UA.indexOf('macintosh') > -1 && 'ontouchend' in document) {
 		$.getJSON('https://fed579.github.io/docs/bnrList_02.json', {
 				ts: new Date().getTime()
 		})
